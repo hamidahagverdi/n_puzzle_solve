c1ef1e76e9d3ff5de42b0f9bdd31be9a5b9baf41
hamida_hagverdiyeva <hamida.hagverdi1@gmail.com>
2025-02-18 18:08:05 +0400

    Update __init__.py

--- src/npuzzle/__init__.py
+++ src/npuzzle/__init__.py
@@ -1,5 +1,6 @@
 from .puzzle import NPuzzle
 from .utils import read_puzzle_from_file
+from .main import main
 
 __version__ = "1.0.0"
-__all__ = ['NPuzzle', 'read_puzzle_from_file']
+__all__ = ['NPuzzle', 'read_puzzle_from_file', 'main']
